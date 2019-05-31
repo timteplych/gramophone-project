6ea6edc5cbd5cef0de282d8a4b9d008536485813
Антон <37295785+AntonBelyaevskiy@users.noreply.github.com>
2019-05-31 17:10:10 +0800

    Update MainController.java

--- gramophone/src/main/java/com/geekbrains/gramophone/controllers/MainController.java
+++ gramophone/src/main/java/com/geekbrains/gramophone/controllers/MainController.java
@@ -3,6 +3,8 @@ package com.geekbrains.gramophone.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+// just test comment
+
 @Controller
 public class MainController {
 
