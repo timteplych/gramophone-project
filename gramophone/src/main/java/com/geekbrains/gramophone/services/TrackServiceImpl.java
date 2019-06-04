cc086b086675a229b8f098c259d35ee83d03d5f0
timteplych <timteplych@mail.ru>
2019-06-04 22:16:06 +0800

    search track by author and genre

--- gramophone/src/main/java/com/geekbrains/gramophone/services/TrackServiceImpl.java
+++ gramophone/src/main/java/com/geekbrains/gramophone/services/TrackServiceImpl.java
@@ -1,6 +1,8 @@
 package com.geekbrains.gramophone.services;
 
+import com.geekbrains.gramophone.entities.Genre;
 import com.geekbrains.gramophone.entities.Track;
+import com.geekbrains.gramophone.repositories.GenreRepository;
 import com.geekbrains.gramophone.repositories.TrackRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -9,6 +11,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 
 @Service
@@ -16,9 +19,16 @@ public class TrackServiceImpl implements TrackService {
 
     private TrackRepository trackRepository;
 
+    private GenreRepository genreRepository;
+
     @Autowired
     public void setTrackRepository(TrackRepository trackRepository) {
         this.trackRepository = trackRepository;
+    }
+
+    @Autowired
+    public void setGenreRepository(GenreRepository genreRepository) {
+        this.genreRepository = genreRepository;
     }
 
     @Override
@@ -34,5 +44,43 @@ public class TrackServiceImpl implements TrackService {
 
     public Page<Track> getTracksWithPagingAndFiltering(int pageNumber, int pageSize, Specification<Track> trackSpecification) {
         return trackRepository.findAll(trackSpecification, PageRequest.of(pageNumber, pageSize));
+    }
+
+    @Override
+    public List<Track> findByTitle(String title) {
+        if("".equals(title)){
+            return Collections.emptyList();
+        }
+        return trackRepository.findAllByTitleContaining(title);
+    }
+
+    @Override
+    public List<Track> findByAuthor(String author) {
+        if("".equals(author)){
+            return Collections.emptyList();
+        }
+        return trackRepository.findAllByMusicAuthorContaining(author);
+    }
+
+    @Override
+    public List<Track> findByGenreTitle(String title) {
+        if("".equals(title)){
+            return Collections.emptyList();
+        }
+        Genre genre = genreRepository.findByTitle(title);
+        if (genre == null){
+            return Collections.emptyList();
+        }
+
+        return trackRepository.findAllByGenre(genre);
+    }
+
+    @Override
+    public List<Track> findByGenreId(long id) {
+        Genre genre = genreRepository.findById(id).orElse(null);
+        if(genre == null){
+            return Collections.emptyList();
+        }
+        return trackRepository.findAllByGenre(genre);
     }
 }
