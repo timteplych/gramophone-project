62285970a1c863f30fc6e8614ed5b988690874bc
alexey <vadi2016@outlook.com>
2019-06-11 10:42:33 +0300

    Added routes

--- gramophone-frontend/src/app/app-routing.module.ts
+++ gramophone-frontend/src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {MainPageComponent} from "./main-page/main-page.component";
+import {MainPageComponent} from './main-page/main-page.component';
+import {TrackPageComponent} from './track-page/track-page.component';
+import {UserAccountComponent} from './user-account/user-account.component';
 
 const routes: Routes = [
   {path: '', component: MainPageComponent},
+  {path: 'tracks', component: TrackPageComponent},
+  {path: 'account', component: UserAccountComponent},
 ];
 
 @NgModule({
