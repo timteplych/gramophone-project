4a42501c885855b1ca8d171c3de198f8595555bc
alexey <vadi2016@outlook.com>
2019-06-11 10:39:27 +0300

    Added registration component with form

---
+++ gramophone-frontend/src/app/register/register.component.ts
@@ -0,0 +1,30 @@
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent implements OnInit {
+
+  registerForm: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+  ) {
+  }
+
+  ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', Validators.required],
+      phone: ['', Validators.required],
+      password: ['', Validators.required],
+      password2: ['', Validators.required]
+    });
+  }
+
+}
