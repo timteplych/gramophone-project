efb23e4a97117abccb295a10a08a68e4f94be4c5
alexey <vadi2016@outlook.com>
2019-06-14 13:26:34 +0300

    Changed routes for animation

--- gramophone-frontend/src/app/app-routing.module.ts
+++ gramophone-frontend/src/app/app-routing.module.ts
@@ -5,9 +5,9 @@ import {TrackPageComponent} from './track-page/track-page.component';
 import {UserAccountComponent} from './user-account/user-account.component';
 
 const routes: Routes = [
-  {path: '', component: MainPageComponent},
-  {path: 'tracks', component: TrackPageComponent},
-  {path: 'account', component: UserAccountComponent},
+  {path: '', component: MainPageComponent, data: {animation: 'Home'}},
+  {path: 'tracks/:id', component: TrackPageComponent, data: {animation: 'Track'}},
+  {path: 'account', component: UserAccountComponent, data: {animation: 'Account'}},
 ];
 
 @NgModule({
