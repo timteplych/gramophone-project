ef917c3d52d59ddcc8c27afe8ad9698b2ef724af
alexey <vadi2016@outlook.com>
2019-06-14 13:49:23 +0300

    Added new method find by email for login method

--- gramophone/src/main/java/com/geekbrains/gramophone/repositories/UserRepository.java
+++ gramophone/src/main/java/com/geekbrains/gramophone/repositories/UserRepository.java
@@ -8,4 +8,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
     User findOneByUsername(String username);
+
+    User findByEmail(String email);
 }
