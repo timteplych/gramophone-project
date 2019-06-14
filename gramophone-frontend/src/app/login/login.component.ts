9ac2a1709b029b29cdec8f7aedcddbb9a87a637c
alexey <vadi2016@outlook.com>
2019-06-14 13:18:24 +0300

    Connected foront and back

--- gramophone-frontend/src/app/login/login.component.ts
+++ gramophone-frontend/src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {AuthenticationService} from '../_services';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,11 +9,16 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
+  loading = false;
+  submitted = false;
+  returnUrl: string;
   loginForm: FormGroup;
 
   constructor(
+    private route: ActivatedRoute,
+    private router: Router,
     private formBuilder: FormBuilder,
+    private authService: AuthenticationService
   ) {
   }
 
@@ -21,6 +28,22 @@ export class LoginComponent implements OnInit {
       email: ['', Validators.required],
       password: ['', Validators.required]
     });
+  }
+
+  get f() {
+    return this.loginForm.controls;
+  }
+
+  onSubmited() {
+    if (this.loginForm.invalid) {
+      return;
+    }
+    this.authService.login(this.f.email.value, this.f.password.value).subscribe(data => {
+      this.loading = true;
+      },
+      error => {
+        this.loading = false;
+      });
   }
 
 }
