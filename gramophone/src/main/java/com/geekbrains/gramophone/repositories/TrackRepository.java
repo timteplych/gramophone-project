666d5dd5f1fc979892fb542051de5d787af229c6
Roman Semenov <rosemenov@yandex.ru>
2019-06-14 22:44:02 +0300

    fix some errors

--- gramophone/src/main/java/com/geekbrains/gramophone/repositories/TrackRepository.java
+++ gramophone/src/main/java/com/geekbrains/gramophone/repositories/TrackRepository.java
@@ -2,8 +2,11 @@ package com.geekbrains.gramophone.repositories;
 
 import com.geekbrains.gramophone.entities.Genre;
 import com.geekbrains.gramophone.entities.Track;
+import com.geekbrains.gramophone.entities.User;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -16,4 +19,6 @@ public interface TrackRepository extends PagingAndSortingRepository<Track, Long>
 
     List<Track> findAllByGenre(Genre genre);
 
+    @Query(value = "select count(*) from tracks_likes tl where track_id = ?1 and user_id = ?2", nativeQuery = true)
+    int trackLikedBy(Long trackId, Long userId);
 }
