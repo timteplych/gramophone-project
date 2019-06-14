bf723a810c219f9c52442e09c2dc12b5037a6302
alexey <vadi2016@outlook.com>
2019-06-14 13:25:28 +0300

    Animations code

--- gramophone-frontend/src/app/app.component.ts
+++ gramophone-frontend/src/app/app.component.ts
@@ -1,10 +1,84 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {animate, group, query, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [trigger('routeAnimations', [
+    transition('Account => *', [
+      query(':enter, :leave',
+        style({opacity: 1}),
+        {optional: true}),
+      group([
+        query(':enter', [
+          style({opacity: 0}),
+          animate('1s ease-in-out',
+            style({opacity: 1}))
+        ], {optional: true}),
+        query(':leave', [
+          style({opacity: 0}),
+          animate('1s ease-in-out',
+            style({opacity: 0}))
+        ], {optional: true}),
+      ])
+    ]),
+    transition('Home => *', [
+      query(':enter, :leave',
+        style({opacity: 1}),
+        {optional: true}),
+      group([
+        query(':enter', [
+          style({opacity: 0}),
+          animate('1s ease-in-out',
+            style({opacity: 1}))
+        ], {optional: true}),
+        query(':leave', [
+          style({opacity: 0}),
+          animate('1s ease-in-out',
+            style({opacity: 1}))
+        ], {optional: true}),
+      ])
+    ]),
+    transition('Track => *', [
+      query(':enter, :leave',
+        style({opacity: 1}),
+        {optional: true}),
+      group([
+        query(':enter', [
+          style({opacity: 0}),
+          animate('1s ease-in-out',
+            style({opacity: 1}))
+        ], {optional: true}),
+        query(':leave', [
+          style({opacity: 0}),
+          animate('1s ease-in-out',
+            style({opacity: 0}))
+        ], {optional: true}),
+      ])
+    ]),
+    transition('Track => Home', [
+      query(':enter, :leave',
+        style({opacity: 1}),
+        {optional: true}),
+      group([
+        query(':enter', [
+          style({opacity: 0}),
+          animate('1s ease-in-out',
+            style({opacity: 1}))
+        ], {optional: true}),
+        query(':leave', [
+            style({opacity: 1}),
+            animate('1s ease-in-out',
+              style({opacity: 0}))
+          ], {optional: true}
+        )]
+      )]
+    )]
+  )]
 })
+
+
 export class AppComponent {
   title = 'gramophone-frontend';
 }
