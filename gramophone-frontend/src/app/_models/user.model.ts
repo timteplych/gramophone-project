c61611e2dcf9f642b6c765a34ed9ff7c666592ac
alexey <vadi2016@outlook.com>
2019-06-25 14:04:10 +0300

    Added a new models and changed some fields in old models

--- gramophone-frontend/src/app/_models/user.model.ts
+++ gramophone-frontend/src/app/_models/user.model.ts
@@ -1,18 +1,23 @@
 import {Role} from './role.model';
+import {Playlist} from './playlist.model';
+import {InfoSinger} from './infoSinger.model';
 
 export class User {
   constructor(
+    public id: number,
     public username: string,
     public email: string,
     public password: string,
     public roles: Role[],
     public token: string,
-    public firstName?: string,
-    public lastName?: string,
-    public phone?: string,
+    public avatar?: string,
+    public activationCode?: string,
+    public infoSinger?: InfoSinger,
+    public playLists?: Playlist[],
+    public subscribers?: User[],
+    public subscription?: User[],
     public createdAt?: bigint,
-    public updatedAt?: bigint,
-    public id?: string
+    public updatedAt?: bigint
   ) {
   }
 }
