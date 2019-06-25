d802dfa0f10512b990ebdffeb7d531024c827608
alexey <vadi2016@outlook.com>
2019-06-25 14:10:45 +0300

    Changed some guards for routes

--- gramophone-frontend/src/app/sidebar/sidebar.component.ts
+++ gramophone-frontend/src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
+import {MatDialog} from "@angular/material";
+import {AuthenticationService} from "../_services";
+import {RoleEnum, User} from "../_models";
 
 declare var $: any;
 
@@ -9,8 +13,18 @@ declare var $: any;
 })
 export class SidebarComponent implements OnInit {
   // faPlayCircle = faPlayCircle;
+  currentUser: User;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private authenticationService: AuthenticationService) {
+
+    this.authenticationService.currentUser.subscribe(value => {
+      this.currentUser = value;
+      this.isUser();
+      this.isAdmin();
+    });
+  }
 
   ngOnInit() {
     $('div.sidebar a.item.accordion')
@@ -19,6 +33,31 @@ export class SidebarComponent implements OnInit {
           trigger: '.title'
         }
       });
+  }
+
+
+  isAdmin(): boolean {
+    let variant = false;
+    if (this.currentUser) {
+      this.currentUser.roles.forEach(obg => {
+        if (obg.name === RoleEnum.Admin) {
+          variant = true;
+        }
+      });
+    }
+    return variant;
+  }
+
+  isUser(): boolean {
+    let variant = false;
+    if (this.currentUser) {
+      this.currentUser.roles.forEach(obg => {
+        if (obg.name === RoleEnum.User) {
+          variant = true;
+        }
+      });
+    }
+    return variant;
   }
 
 }
